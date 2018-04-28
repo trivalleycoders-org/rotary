eab2502d9557f58e5a1418daaa5525cae658fad5
klequis <klequis@gmail.com>
2018-04-27 19:17:20 -0700

    not using yet

---
+++ server/tests/member-utils.js
@@ -0,0 +1,15 @@
+import { yellow, blue, green, red } from '../../logger/'
+
+export const fillMember = (model, data) => {
+  let m = model
+  const d = data
+  m.firstName = d.firstName
+  m.lastName = d.lastName
+  m.email = d.email
+  d.comments && m.comments.push(...d.comments)
+  d.phone && m.phone.push(...d.phone)
+  d.avoidRoles && m.avoidRoles.push(...d.avoidRoles)
+  return m
+}
+
+export default { fillMember }

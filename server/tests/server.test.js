0be341bb7e43db933d602498cf7902c57478bf64
klequis <klequis@gmail.com>
2018-04-27 19:17:03 -0700

    managing mongo

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,76 +5,79 @@ import 'babel-polyfill'
 // import { app, closeDb } from '../server.js'
 import  Member from  '../../models/member'
 import { ObjectID } from 'mongodb'
-import { yellow, blue, green } from '../../logger/'
+import { yellow, blue, green, red } from '../../logger/'
 import app from '../server'
-import { disconnectFromMongo } from '../../db'
+// import { disconnectFromMongo } from '../../db'
+import { twoTestMembers } from './test-users'
+import { oneTestMember } from './test-users'
+import { fillMember } from './member-utils'
 
-
-beforeAll(async () => {
-  green('beforeAll')
+beforeAll(() => {
+  // console.clear()
+  // green('beforeAll')
   // await app.startServer()
   // yellow('app', app())
   // app = await require('../server')
 })
 
-beforeEach(function() {
-  green('beforeEach')
-  console.log('before every test in every file')
+beforeEach(async (done) => {
+  // green('beforeEach')
+  try {
+    const a = await Member.remove({})
+    yellow('a: Member.remove', a)
+    done()
+  }
+  catch (e) {
+    red('ERROR: beforeEach: ', e)
+    // done()
+  }
 })
 
-afterAll( async () => {
-  green('afterAll')
+afterAll(() => {
+  // green('afterAll')
   // closeDb()
-  process.exit(0)
+  // process.exit(0)
 })
 
 describe('Some test', () => {
+  // yellow('Some test')
   it('should = 1', () => {
+    // yellow('*****************************************************')
     expect(1).toBe(1)
   })
 })
 
-const testMembers = [
-  {
-    _id: new ObjectID(),
-    firstName: 'Jane',
-    lastName: 'Doe',
-    comments: ['comment 1', 'comment 2'],
-    email: 'jane@doe.com',
-    phone: {
-      phoneType: 'Mobile',
-      phoneNumber: '222-222-2222',
-    }
-  }, {
-    _id: new ObjectID(),
-    firstName: 'Jon',
-    lastName: 'Snow',
-    comments: ['comment 3', 'comment 4'],
-    email: 'jon@snow.com',
-    phone: {
-      phoneType: 'Work',
-      phoneNumber: '333-333-3333',
-    }
-  }
-]
+describe('GET /members', async () => {
+  const m = await new Member()
+  const member = fillMember(m, oneTestMember[0])
+  yellow('*************member*************', member)
+
+  // const doc = await Member.populate(oneTestMember)
+  // should get all members
+  it('should get all members', (done) => {
+
+    request(app).get('/members').expect(200).expect((res) => {
+      yellow('res.body', res.body)
+      expect(res.body.members.length).toBe(2)
+    }).end(done)
+  })
+})
 
 describe('POST /members', () => {
   it('should create a new member', (done) => {
-
-    // const firstName = 'Jim'
     request(app).post('/members')
-    .send(testMembers[0])
+    .send(twoTestMembers[0])
     .expect(200)
     .expect((res) => {
-      expect(res.body.firstName).toBe(testMembers[0].firstName)
+      expect(res.body.firstName).toBe(twoTestMembers[0].firstName)
     }).end((err, res) => {
       if (err) {
         red(err)
         return done(err)
       }
-      Member.find({firstName: testMembers[0].firstName}).then((members) => {
-        // expect(members.length).toBe(1)
-        // expect(members[0].firstName).toBe(testMembers[0].firstName)
+      Member.find({firstName: twoTestMembers[0].firstName}).then((members) => {
+        expect(members.length).toBe(1)
+        expect(members[0].firstName).toBe(twoTestMembers[0].firstName)
         done()
       }).catch((e) => {
         console.log(e)
